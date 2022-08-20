d4663c03da0d6b4284b6437e44dbc4f1b6e980a9
Parra14 <ofparra69@gmail.com>
2022-08-19 20:42:29 -0500

    Update app.module.ts

--- Productos/cliente/src/app/app.module.ts
+++ Productos/cliente/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+//componentes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
@@ -14,7 +17,8 @@ import { ListarProductosComponent } from './components/listar-productos/listar-p
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
